3650aba670276b5a586c8bf7ba4001466cc907bf
Jamie Lentin <jm@lentin.co.uk>
2022-07-27 15:45:15 +0100

    timefile: Only include final_year_steps if relevant
    If it matches step_lengths, the default will do fine.

--- R/timefile.R
+++ R/timefile.R
@@ -2,14 +2,11 @@ g2to3_timefile <- function (path, file_name) {
     g2_time <- Rgadget::read.gadget.file(path, file_name, 'time', recursive = FALSE)
     if (length(g2_time[[1]]$firststep) == 1 && g2_time[[1]]$firststep != 1) stop("firststep must be 1 (initial part-years not supported)")
     
-    substitute(
-        actions_time <- list(g3a_time(
-            firstyear,
-            lastyear,
-            step_lengths = step_lengths,
-            final_year_steps = final_year_steps)), list(
-        firstyear = g2_time[[1]]$firstyear,
-        lastyear = g2_time[[1]]$lastyear,
-        step_lengths = tail(g2_time[[1]]$notimesteps, -1),
-        final_year_steps = g2_time[[1]]$laststep))
+    out <- call("g3a_time", g2_time[[1]]$firstyear, g2_time[[1]]$lastyear)
+    out[['step_lengths']] <- tail(g2_time[[1]]$notimesteps, -1)
+
+    # Only include laststep if it will differ from the default
+    if (g2_time[[1]]$laststep != length(out[['step_lengths']])) out[['final_year_steps']] <- g2_time[[1]]$laststep
+
+    substitute(actions_time <- list(out), list(out=out))
 }
