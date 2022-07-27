7359d73f631409bd018f65f086caf72595be326d
Jamie Lentin <jm@lentin.co.uk>
2022-07-27 15:44:21 +0100

    formula: De-formula constants
    Wrapping a constant in a formula is pointless, and allows
    g3a_maturity_constant() to pick out the unused values.

--- R/formula.R
+++ R/formula.R
@@ -2,5 +2,10 @@ g2to3_formula <- function (g2f) {
     out <- if (is.call(g2f)) g2f else if (is.na(g2f)) quote(NA) else Rgadget::parse.gadget.formulae(g2f)
     subs <- lapply(all.vars(out), function (x) substitute(g3_param(x), list(x=x)))
     names(subs) <- all.vars(out)
-    gadget3:::f_substitute(out, subs)
+    out <- gadget3:::f_substitute(out, subs)
+
+    # It's a numeric constant, no point being a formula
+    if (length(out) == 2 && is.numeric(out[[2]])) out <- out[[2]]
+
+    return(out)
 }
