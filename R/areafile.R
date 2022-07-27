fdeabc4502f21533661cac387f1da4c87b0ff052
Jamie Lentin <jm@lentin.co.uk>
2022-07-27 15:43:35 +0100

    areafile: area_names doesn't need to be a list
    It's a character vector. A list generally works but is a bit confusing.

--- R/areafile.R
+++ R/areafile.R
@@ -1,7 +1,7 @@
 g2to3_areafile <- function (path, file_name) {
     g2_area <- Rgadget::read.gadget.file(path, file_name, 'area', recursive = FALSE)
 
-    area_names <- as.call(c( as.symbol("list"), as.list(as.character(g2_area[[1]]$areas)) ))
+    area_names <- as.call(c( as.symbol("c"), as.list(as.character(g2_area[[1]]$areas)) ))
     
     return(substitute({
         area_names <- structure(seq_len(area_len), names = area_name_vals)
