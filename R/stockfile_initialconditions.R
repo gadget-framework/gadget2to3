32bcafa09cde0522abd03f20b17a167bb80bba2e
Jamie Lentin <jm@lentin.co.uk>
2022-08-26 10:33:19 +0100

    stockfile_initialconditions: Support normalcond
    normalparam but uses the refweight file.

--- R/stockfile_initialconditions.R
+++ R/stockfile_initialconditions.R
@@ -7,6 +7,8 @@ stockfile_initialconditions <- function (path, stock_var, sect, g2_stock) {
 
     if ('normalparamfile' %in% names(sect)) {
         stockfile_initialconditions_normalparam(path, stock_var, sect, g2_stock)
+    } else if ('normalcondfile' %in% names(sect) || 'initstockfile' %in% names(sect)) {
+        stockfile_initialconditions_normalcond(path, stock_var, sect, g2_stock)
     } else {
         stop('Unknown initial conditions type ', sect)
     }
@@ -59,6 +61,64 @@ stockfile_initialconditions_normalparam <- function (path, stock_var, sect, g2_s
         lapply(npf[,'beta'], function(x) g2to3_formula(path, x)),
         age = as.integer(npf[, 'age']),
         area = area_syms)
+
+    return(out)
+}
+
+# normalcond is normalparam but with a reference weight file to use
+stockfile_initialconditions_normalcond <- function (path, stock_var, sect, g2_stock) {
+    if ('normalcondfile' %in% names(sect)) {
+        npf <- Rgadget::read.gadget.file(path, sect$normalcondfile, 'data', recursive = FALSE)[[1]]
+    } else {
+        npf <- Rgadget::read.gadget.file(path, sect$initstockfile, 'data', recursive = FALSE)[[1]]
+    }
+    if (!identical(names(npf), c('age', 'area', 'age.factor', 'area.factor', 'mean', 'stddev', 'condition'))) {
+        stop("Unknown columns in ", sect$normalparamfile, ": ", paste(names(npf), collapse = ", "))
+    }
+
+    # Generate area symbols to use when generating if statements
+    area_syms <- lapply(npf[, 'area'], function(x) as.symbol(paste0('area_', x)))
+
+    age_factor_f <- lapply(seq_len(nrow(npf)), function (i) {
+        modify_fns <- list(function (num) as.symbol("age"))
+        names(modify_fns) <- npf[i, 'age']
+        call_replace(g2to3_formula(path, npf[i, 'age.factor'][[1]]), modify_fns)
+    })
+    age_factor_f <- combine_formulas(age_factor_f, age = as.integer(npf[, 'age']), area = area_syms)
+
+    area_factor_f <- lapply(seq_len(nrow(npf)), function (i) {
+        modify_fns <- list(function (num) as.symbol("area"))
+        names(modify_fns) <- npf[i, 'area']
+        call_replace(g2to3_formula(path, npf[i, 'area.factor'][[1]]), modify_fns)
+    })
+    area_factor_f <- combine_formulas(area_factor_f, age = as.integer(npf[, 'age']), area = area_syms)
+
+    out <- call("g3a_initialconditions_normalparam", stock_var)
+    out[['factor_f']] <- gadget3:::f_substitute(
+        quote( age_factor_f * area_factor_f ),
+        list(age_factor_f = age_factor_f, area_factor_f = area_factor_f))
+
+    out[['mean_f']] <- combine_formulas(
+        lapply(npf[,'mean'], function(x) g2to3_formula(path, x)),
+        age = as.integer(npf[, 'age']),
+        area = area_syms)
+
+    out[['stddev_f']] <- combine_formulas(
+        lapply(npf[,'stddev'], function(x) g2to3_formula(path, x)),
+        age = as.integer(npf[, 'age']),
+        area = area_syms)
+
+    condition <- combine_formulas(
+        lapply(npf[,'condition'], function(x) g2to3_formula(path, x)),
+        age = as.integer(npf[, 'age']),
+        area = area_syms)
+    out[['alpha_f']] <- substitute(
+        ~refweight_var * condition,
+        list(
+            refweight_var = as.symbol(paste0(stock_var, "_refweight")),
+            condition = condition))
+
+    out[['beta_f']] <- 0
 
     return(out)
 }
